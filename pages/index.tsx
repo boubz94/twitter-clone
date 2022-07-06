2bb63b0ea8e062063941ffc7f29ab78bf1d2deae
Boubou <abdel.mekchouche@gmail.com>
2022-07-07 00:44:39 +0200

    fetch tweet from sanity

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,19 @@
-import type { NextPage } from "next";
+import type { GetServerSideProps, NextPage } from "next";
 import Head from "next/head";
 import Image from "next/image";
 import Feed from "../components/Feed";
 import SideBar from "../components/SideBar";
 import Widget from "../components/Widget";
+import { Tweet } from "../typings";
+import { fetchTweets } from "../utils/fetchTweets";
 
-const Home: NextPage = () => {
+interface Props {
+  tweets: Tweet[];
+}
+
+const Home = ({ tweets }: Props) => {
+  console.log(tweets);
+
   return (
     <div className="lg:max-w-6xl mx-auto max-h-screen overflow-hidden">
       <Head>
@@ -24,9 +32,11 @@ const Home: NextPage = () => {
 
 export default Home;
 
-export async function getServerSideProps(context) {
+export const getServerSideProps: GetServerSideProps = async (context) => {
   const tweets = await fetchTweets();
   return {
-    props: {}, // will be passed to the page component as props
+    props: {
+      tweets,
+    }, // will be passed to the page component as props
   };
-}
+};
